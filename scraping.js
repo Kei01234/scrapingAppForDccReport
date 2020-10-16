a3a4a226942e60e3ffe6f139729aa84430377974
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-10-17 02:29:41 +0900

    開始値なども出力

--- scraping.js
+++ scraping.js
@@ -3,19 +3,25 @@ const { JSDOM } = require("jsdom");
 
 const filename = "sample";
 const separator = "\t";
-const xyPattern = /^x: (.*), y: (.*)$/;
+const pointPattern = /^.*: (.*), .*: (.*)$/;
 
 const html = fs.readFileSync(`${filename}.html`);
 const { window } = new JSDOM(html);
+
+const points = window.document
+  .querySelector(".graph-info")
+  .innerHTML.split("<br>");
+const info = points.map((point) => point.match(pointPattern).slice(1, 3));
+
 // see also: https://stackoverflow.com/questions/3772290/css-selector-that-applies-to-elements-with-two-classes
 const notes = [...window.document.querySelectorAll(".note:not(.new)")];
-
-const header = ["memo", "x", "y"].join(separator);
 const contents = notes.map((note) => {
   const memo = note.childNodes[2].value;
-  const [, x, y] = note.childNodes[3].textContent.match(xyPattern);
+  const [, x, y] = note.childNodes[3].textContent.match(pointPattern);
   return [memo, x, y].join(separator);
 });
 
+const header = ["memo", "x", "y"].join(separator);
 const tsv = [header, ...contents].join("\n");
 fs.writeFileSync(`${filename}.tsv`, tsv);
+console.log(info.flat().join(" "));
